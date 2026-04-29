b62530739c3fdd771ae96ea5596ccb7b038f3fc3
Alumno <Alumno@172.16.0.210>
2026-04-29 12:49:11 -0300

    Todo metido para un commit final

--- src/main/java/Ejercicio/Main.java
+++ src/main/java/Ejercicio/Main.java
@@ -1,12 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
 package Ejercicio;
 
-/**
- *
- * @author Alumno
- */
+
 public class Main {
 
     public static void main(String[] args) {
